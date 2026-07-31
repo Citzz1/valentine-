caa13cf48adc382f7cd3ae7bcadcb53471100f99
Citzz1 <mcjimfikri@gmail.com>
2026-07-31 22:29:45 +0700

    Update metadata title and description for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ const playfair = Playfair_Display({
 });
 
 export const metadata: Metadata = {
-  title: "Happy Valentine L",
-  description: "A special Valentine's gift for L ❤️",
+  title: "Happy Girlfriend Pipit",
+  description: "A special Girlfriend gift for Pipitt ❤️",
 };
 
 export default function RootLayout({
